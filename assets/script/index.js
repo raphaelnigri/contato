75e8afabcd41411b8fe69a91150107338a522e22
magusnigri <link_nigri@hotmail.com>
2022-10-17 19:58:59 -0300

    update

---
+++ assets/script/index.js
@@ -0,0 +1,75 @@
+const mail = document.querySelectorAll('.email')
+const tel = document.querySelectorAll('.telefone')
+const whats = document.querySelectorAll('.whatsapp')
+const input = document.querySelectorAll("input[type=radio]");
+
+function mailON (){
+	if (mail[0].classList.contains('off')) {
+		for(let i of mail) {  
+			i.classList.remove('off');
+		}
+		mail[1].required = true;
+	}
+}
+function mailOff(){
+	if (!mail[0].classList.contains('off')) {
+		for(let i of mail) {  
+			i.classList.add('off');
+		}
+		mail[1].required = false;
+	}
+}
+
+function tellON (){
+	if (tel[0].classList.contains('off')) {
+		for(let i of tel) {  
+			i.classList.remove('off');
+		}
+		tel[1].required = true;
+	}
+}
+function tellOff(){
+	if (!tel[0].classList.contains('off')) {
+		for(let i of tel) {  
+			i.classList.add('off');
+		}
+		tel[1].required = false;
+	}
+}
+
+function whatsON (){
+	if (whats[0].classList.contains('off')) {
+		for(let i of whats) {  
+			i.classList.remove('off');
+		}
+		whats[1].required = true;
+	}
+}
+function whatsOff(){
+	if (!whats[0].classList.contains('off')) {
+		for(let i of whats) {  
+			i.classList.add('off');
+		}
+		whats[1].required = false;
+	}
+}
+
+for (let i of input){
+	i.addEventListener('change', () => {
+		if (input[0].checked){
+			mailON();
+			tellOff();
+			whatsOff();
+		}
+		if (input[1].checked){
+			mailOff();
+			tellON();
+			whatsOff();
+		}
+		if (input[2].checked){
+			mailOff();
+			tellOff();
+			whatsON();
+		}
+	})
+}
