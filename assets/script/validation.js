5519a803d2262d872a9473c8da82a21edec04057
magusnigri <link_nigri@hotmail.com>
2022-10-27 03:23:58 -0300

    Update validation.js

--- assets/script/validation.js
+++ assets/script/validation.js
@@ -13,17 +13,18 @@ const mensagensDeErro = {
     },
     cpf:{
         valueMissing: 'Informe o seu CPF ou CNPJ.',
-        patternMismatch: 'Isso não é um CPF ou CNPJ'
+        patternMismatch: 'Isso não é um CPF ou CNPJ.',
+        customError: 'O CPF ou CNPJ digitado não é válido.'
     },
     site:{
-        typeMismatch: 'Esse url não é válido. Tente colocar http: no início do campo'
+        typeMismatch: 'Esse url não é válido. Tente colocar http: no início do campo.'
     },
     email:{
-        valueMissing: 'O campo de email deve ser preenchido',
-        typeMismatch: 'Isso não é um email'
+        valueMissing: 'O campo de email deve ser preenchido.',
+        typeMismatch: 'Isso não é um email.'
     },
     telefone:{
-        valueMissing: 'Informe seu telefone ou celular',
+        valueMissing: 'Informe seu telefone ou celular.',
         patternMismatch: 'Esse numero não é válido, tente dessa forma: (ddd) xxxx-xxxx'
     },
     whatsapp:{
@@ -36,6 +37,7 @@ const erros = [
     'valueMissing',
     'typeMismatch',
     'patternMismatch',
+    'customError'
 ]
 
 function mostraAlerta(inputType , input){
@@ -74,6 +76,38 @@ inputs.forEach(input =>{
     })
 })
 
-function validaCPF(){
+function validaCPF(input){
+    const cpfFormatado = input.value.replace(/\D/g, ''); /*remove tudo que não é número do cpf*/
+    let mensagem = '';
 
+    if(!checaCPFRepetido(cpfFormatado)){
+        mensagem= 'O CPF digitado não é válido.'
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function checaCPFRepetido(cpf){
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+    ];
+    
+    let cpfValido = true;
+
+    valoresRepetidos.forEach(valor =>{
+        if(valor == cpf){
+            cpfValido = false;
+        }
+    });
+
+    return cpfValido;
 }
